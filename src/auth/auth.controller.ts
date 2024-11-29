3081a9f4270b62d406d74028c2ba297bfcdaa1be
Alexander <sashakypinko@gmail.com>
2024-11-29 10:19:12 +0100

    added login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import TYPES from '../types';
 import AuthService from './auth.service';
 import {AuthResponseDto} from './dto/auth-response.dto';
 import {RegisterDto} from './dto/register.dto';
+import {LoginDto} from './dto/login.dto';
 
 @Controller('/auth')
 class AuthController {
@@ -13,6 +14,12 @@ class AuthController {
   @Post('register')
   register(@Body() data: RegisterDto): Promise<AuthResponseDto> {
     return this.authService.register(data);
+  }
+  
+  @ValidateDto(LoginDto)
+  @Post('login')
+  login(@Body() data: LoginDto): Promise<AuthResponseDto> {
+    return this.authService.login(data);
   }
 }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,17 @@
-import {BadRequestException, Injectable} from 'light-kite';
+import {BadRequestException, HttpException, Injectable} from 'light-kite';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import {IUser, User} from './user.schema';
 import {AuthResponseDto} from './dto/auth-response.dto';
 import {RegisterDto} from './dto/register.dto';
+import {LoginDto} from './dto/login.dto';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'secret';
 
 @Injectable()
 class AuthService {
-  async register(userData: RegisterDto): Promise<AuthResponseDto> {
-    const existingUser: IUser | null = await User.findOne({ username: userData.username });
+  async register(registerData: RegisterDto): Promise<AuthResponseDto> {
+    const existingUser: IUser | null = await User.findOne({ username: registerData.username });
 
     if (existingUser) {
       throw new BadRequestException('User with this username already exists.');
@@ -19,11 +20,21 @@ class AuthService {
     const salt = await bcrypt.genSalt(10);
 
     const user = new User({
-      ...userData,
-      password: await bcrypt.hash(userData.password, salt)
+      ...registerData,
+      password: await bcrypt.hash(registerData.password, salt)
     });
     await user.save();
     
+    return this.authorize(user);
+  }
+  
+  async login(loginData: LoginDto): Promise<AuthResponseDto> {
+    const user: IUser | null = await await User.findOne({ username: loginData.username });
+    if (!user) throw new BadRequestException('Invalid credentials');
+
+    const isMatch = await bcrypt.compare(loginData.password, user.password);
+    if (!isMatch) throw new BadRequestException('Invalid credentials');
+
     return this.authorize(user);
   }
 
